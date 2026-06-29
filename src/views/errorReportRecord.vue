a6e1df2e3f5904b9a27580226fd4b09384629b43
liuning <2088086564@qq.com>
2026-06-29 20:19:13 +0800

    1

--- src/views/errorReportRecord.vue
+++ src/views/errorReportRecord.vue
@@ -12,8 +12,6 @@
           <span>异常类型</span>
           <el-select v-model="recordType" placeholder="请选择异常类型">
             <el-option label="读数异常" value="report" />
-            <el-option label="报文解析失败" value="报文解析失败" />
-            <el-option label="表号不存在" value="表号不存在" />
           </el-select>
         </div>
         <div class="search-input" style="margin-left: 10px" v-if="companyId === 1">
